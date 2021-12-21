0d6393dc187e606236f19e241ca1c8e1c784fef0
Shiromage <48655796+Shiromage@users.noreply.github.com>
2021-12-20 19:51:46 -0600

    Switch Debounce
    - Added a timer for each button to help debounce them.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #define COLOR_CYCLE_BUTTON_PIN  4
 #define EFFECT_BUTTON_PIN       6
 #define BRIGHTNESS_BUTTON_PIN   8
+#define BUTTON_DEBOUNCE_COOLDOWN_MS 40
+
+volatile unsigned long brightness_button_debounce_alarm, color_button_debounce_alarm, effect_button_debounce_alarm;
 
 Adafruit_NeoPixel Stage;
 
@@ -40,15 +43,13 @@ void setup()
     {
         while(1);
     }
+    brightness_button_debounce_alarm = color_button_debounce_alarm = effect_button_debounce_alarm = 0;
 }
 
-volatile bool changeEffectFlag = false;
-volatile bool changeBrightnessFlag = false;
-volatile bool changeColorFlag = false;
-long buttonCooldown = 0;
 void loop()
 {
     static unsigned long last_millis = 0;
+    unsigned long current_millis = millis();
 
     //Poll button pins for changes if no interrupt is attached to them.
     /*#if (digitalPinToInterrupt(BRIGHTNESS_BUTTON_PIN) == NOT_AN_INTERRUPT)
@@ -60,39 +61,49 @@ void loop()
     #if (digitalPinToInterrupt(EFFECT_BUTTON_PIN) == NOT_AN_INTERRUPT)
     changeEffectFlag = !digitalRead(EFFECT_BUTTON_PIN);
     #endif*/
+    const unsigned long brightness_alarm = brightness_button_debounce_alarm;
+    const unsigned long color_alarm = color_button_debounce_alarm;
+    const unsigned long effect_alarm = effect_button_debounce_alarm;
 
-    if(changeBrightnessFlag)
+    if(brightness_alarm != 0 && brightness_alarm <= current_millis)
     {
-        Serial.println("Changing Brightness...");
-        if(++BrightnessIndex >= sizeof(BrightnessLevels) / sizeof(const int))
+        if(!digitalRead(BRIGHTNESS_BUTTON_PIN))
         {
-            BrightnessIndex = 0;
+            Serial.println("Changing Brightness...");
+            if(++BrightnessIndex >= sizeof(BrightnessLevels) / sizeof(const int))
+            {
+                BrightnessIndex = 0;
+            }
+            Stage.setBrightness(BrightnessLevels[BrightnessIndex]);
         }
-        Stage.setBrightness(BrightnessLevels[BrightnessIndex]);
-        changeBrightnessFlag = false;
+        brightness_button_debounce_alarm = 0;
     }
-    if(changeColorFlag)
+    if(color_alarm != 0 && color_alarm <= current_millis)
     {
-        Serial.println("Changing Color...");
-        if(Effects[CurrentEffectIndex]->changeColor != NULL)
-            Effects[CurrentEffectIndex]->changeColor();
-        changeColorFlag = false;
-    }
-    if(changeEffectFlag)
-    {
-        Serial.println("Changing Effect...");
-        Stage.clear();
-        if(++CurrentEffectIndex >= sizeof(Effects) / sizeof(struct effect_s *))
+        if(!digitalRead(COLOR_CYCLE_BUTTON_PIN))
         {
-            CurrentEffectIndex = 0;
+            Serial.println("Changing Color...");
+            if(Effects[CurrentEffectIndex]->changeColor != NULL)
+                Effects[CurrentEffectIndex]->changeColor();
         }
-        Effects[CurrentEffectIndex]->init(&Stage);
-        last_millis = millis();
-        changeEffectFlag = false;
-        return;
+        color_button_debounce_alarm = 0;
     }
-
-    unsigned long current_millis = millis();
+    if(effect_alarm != 0 && effect_alarm <= current_millis)
+    {
+        if(!digitalRead(EFFECT_BUTTON_PIN))
+        {
+            Serial.println("Changing Effect...");
+            Stage.clear();
+            if(++CurrentEffectIndex >= sizeof(Effects) / sizeof(struct effect_s *))
+            {
+                CurrentEffectIndex = 0;
+            }
+            Effects[CurrentEffectIndex]->init(&Stage);
+            last_millis = current_millis;
+            current_millis = millis();
+        }
+        effect_button_debounce_alarm = 0;
+    }
 
     Stage.clear();
     Effects[CurrentEffectIndex]->step(last_millis - current_millis);
@@ -101,27 +112,22 @@ void loop()
     last_millis = current_millis;
 }
 
-void changeBrightness()
-{
-    changeBrightnessFlag = true;
-}
-
-void changeColor()
-{
-    changeColorFlag = true;
-}
-
-void changeEffect()
-{
-    changeEffectFlag = true;
-}
-
 void handleButtons()
 {
-    changeBrightnessFlag = !digitalRead(BRIGHTNESS_BUTTON_PIN);
-    changeColorFlag = !digitalRead(COLOR_CYCLE_BUTTON_PIN);
-    changeEffectFlag = !digitalRead(EFFECT_BUTTON_PIN);
-    Serial.println("Button press detected.");
+    unsigned long alarm_time = millis() + BUTTON_DEBOUNCE_COOLDOWN_MS;
+    if(!digitalRead(BRIGHTNESS_BUTTON_PIN))
+    {
+        brightness_button_debounce_alarm = alarm_time;
+    }
+    else if(!digitalRead(COLOR_CYCLE_BUTTON_PIN))
+    {
+        color_button_debounce_alarm = alarm_time;
+    }
+    else if(!digitalRead(EFFECT_BUTTON_PIN))
+    {
+        effect_button_debounce_alarm = alarm_time;
+    }
+    //Serial.println("Button press detected.");
 }
 
 void setupButtons()
